ab167c113f788bf3a43b83adde8040ab82fd9309
MathiasFM <96435651+MathiasFM@users.noreply.github.com>
2024-03-21 00:52:13 +0100

    Update fast-start.sh

--- scripts/fast-start.sh
+++ scripts/fast-start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+SCRIPT_PATH="~/easy-setup/scripts"
+
 sudo apt update -y
 sudo apt upgrade -y
 
-bash ~/easy-setup/scripts/brew-install.sh
+bash ${SCRIPT_PATH}/brew-install.sh
 
 source ~/.bashrc
 
-bash ~/easy-setup/scripts/go-install.sh
+bash ${SCRIPT_PATH}/go-install.sh
 
 source ~/.bashrc
 
-bash ~/easy-setup/scripts/k9s-install.sh
+bash ${SCRIPT_PATH}/k9s-install.sh
+
+source ~/.bashrc
+
+bash ${SCRIPT_PATH}/ks-install.sh
 
 source ~/.bashrc
