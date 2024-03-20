714d06e7a06b106f9b2c87ccdcd9ae9f408a2336
MathiasFM <96435651+MathiasFM@users.noreply.github.com>
2024-03-20 23:24:51 +0100

    Create ghcr_secret.sh

---
+++ k8s/deploy/ghcr_secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry github-container-registry \
+  --docker-server=ghcr.io \
+  --docker-username=Srams-Bertha \
+  --docker-password=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 \
+  --docker-email=srams@cyber-wizard.com
