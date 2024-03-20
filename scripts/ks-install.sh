246f64777abe4047525f4cb29ca0612913bf220f
MathiasFM <96435651+MathiasFM@users.noreply.github.com>
2024-03-21 00:54:43 +0100

    Create ks-install.sh

---
+++ scripts/ks-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo '
+ks() {
+  bold=$(tput bold)
+  normal=$(tput sgr0)
+  selected=$(ls "${HOME}/.kube/configs/" | xargs -n 1 basename | fzf --height 25% --prompt "Select kubeconfig> " --border)
+  if [ $? -eq 0 ]; then
+    echo "${bold}Selected kubeconfig:${normal} ${selected}"
+    export KUBECONFIG="${HOME}/.kube/configs/${selected}"
+  fi
+}' >> ~/.bashrc
