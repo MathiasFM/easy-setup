eaee7828a92ee2543d013b0d27cf27c1458fbc57
MathiasFM <96435651+MathiasFM@users.noreply.github.com>
2024-03-20 23:45:34 +0100

    Update fast-start.sh

--- scripts/fast-start.sh
+++ scripts/fast-start.sh
@@ -5,7 +5,13 @@ sudo apt upgrade -y
 
 bash ~/easy-setup/scripts/brew-install.sh
 
+source .bashrc
+
 bash ~/easy-setup/scripts/go-install.sh
+
+source .bashrc
+
+sudo apt-get install build-essential procps curl file git -y
 
 brew install derailed/k9s/k9s
 
