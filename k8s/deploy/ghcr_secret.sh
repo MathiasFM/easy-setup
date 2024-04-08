87451f81d5647865be834fac36e59e4d27f43b4f
MathiasFM <96435651+MathiasFM@users.noreply.github.com>
2024-04-08 23:32:35 +0200

    Delete k8s/deploy/ghcr_secret.sh

--- k8s/deploy/ghcr_secret.sh
+++
@@ -1,5 +0,0 @@
-kubectl create secret docker-registry github-container-registry \
-  --docker-server=<registry>[ghcr.io] \
-  --docker-username=<username> \
-  --docker-password=<registry-token> \
-  --docker-email=<e-mail>
